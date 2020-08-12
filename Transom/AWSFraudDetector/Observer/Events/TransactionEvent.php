818bde994a9c0672ae18365974d297f3b66e59cb
vagrant <vagrant@dev.m2.local.com>
2020-08-12 02:06:30 +0000

    Inital check-in of the AWS Fraud Detection module.

---
+++ Transom/AWSFraudDetector/Observer/Events/TransactionEvent.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Transom Group Inc.
+ *
+ *
+ * @category    Transom
+ * @package     Transom_Group
+ * @copyright   Copyright (c) Transom Group. All rights reserved. (https://transom-group.com/)
+ */
+
+namespace Transom\AWSFraudDetector\Observer\Events;
+
+use Magento\Framework\Event\ObserverInterface;
+use Psr\Log\LoggerInterface;
+
+class TransactionEvent implements ObserverInterface
+{
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $this->logger->info('>>>> In Transom AWSFraudDetector TransactionEvent <<<<<');
+    }
+}
