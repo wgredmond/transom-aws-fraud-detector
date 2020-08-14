8c5c37d50b1f01084eaeb60831d8009d5edc6074
vagrant <vagrant@dev.m2.local.com>
2020-08-14 01:17:31 +0000

    added start of admin ui; added start of api calls.

--- Transom/AWSFraudDetector/Observer/Events/CreateOrderEvent.php
+++ Transom/AWSFraudDetector/Observer/Events/CreateOrderEvent.php
@@ -12,6 +12,8 @@ namespace Transom\AWSFraudDetector\Observer\Events;
 
 use Magento\Framework\Event\ObserverInterface;
 use Psr\Log\LoggerInterface;
+use Aws\FraudDetector\FraudDetectorClient;
+
 
 class CreateOrderEvent implements ObserverInterface
 {
@@ -33,5 +35,36 @@ class CreateOrderEvent implements ObserverInterface
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $this->logger->info('>>>> In Transom AWSFraudDetector CreateOrderEvent <<<<<');
+        $this->logger->info('0.2');
+        /*
+         * admin
+         * AKIAVBHZLHZR72NGUNPH
+         * CIv+STPEq36gmRUi8ACLKeXtibbFnFfyGzYgD79P
+         */
+        $client = new FraudDetectorClient([
+            'version' => 'latest',
+            'region'  => 'us-east-1',
+            'credentials' => [
+                'key'    => 'AKIAVBHZLHZR72NGUNPH',
+                'secret' => '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'
+            ]
+        ]);
+        $this->logger->info('1');
+        //$this->logger->info($client);
+
+        $result = $client->GetDetectors([
+            'detectorId' => 'crs_demo_order', // REQUIRED
+            'maxResults' => 10
+        ]);
+        $this->logger->info('2');
+        $this->logger->info($result);
+
+        /*
+        {
+            "detectorId": "string",
+           "maxResults": number,
+           "nextToken": "string"
+        }
+        */
     }
 }
